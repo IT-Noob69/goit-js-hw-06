3e407cd0159ec149a75c5dbac645ff7f68e51398
IT-Noob69 <122233921+IT-Noob69@users.noreply.github.com>
2023-08-13 23:47:19 +0300

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector(`#ingredients`);
+ingredients.map(ingredient => {
+  const ingredientEl = document.createElement(`li`);
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add(`item`);
+  ingredientsList.append(ingredientEl);
+  // console.log(ingredientEl);
+});
+
